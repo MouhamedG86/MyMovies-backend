0b2d0ac9760a9241fac1546b991131c8fb0d8fda
mouhamed gueye <gueyenioro2012@gmail.com>
2024-02-07 16:31:48 +0100

    test

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config
+var express = require('express');
+var router = express.Router();
+var fetch = require('node-fetch')
+
+router.get('/movies', (req, res) => {
+
+    const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+    const options = {
+        method: 'GET',
+        headers: {
+        accept: 'application/json',
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Njg2NjU5NDE4ODJjM2U0ZjczNmEwYzI2ODgzNzk4YSIsInN1YiI6IjY1YzM0NzdiYjc2Y2JiMDE4NDBmZjg3MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wr9O8oOZMzYmqLEcQRAue4zmIlwlNFUoj9Znq5GwdIQ`
+        }
+    };
+
+    fetch(url, options)
+        .then(res => res.json())
+        .then(json => res.json({ movies: json.results }))
+        .catch(err => console.error('error:'+err));
+})
+
+module.exports = router;
